bfff30b032ba2398bbe96ee589e400f5273ac02a
Esteban Gabriel Fernandez <esteban.fernandez93@outlook.com.ar>
2019-02-16 07:44:16 -0300

    Service Workers agregado!

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -36,6 +36,12 @@ const theme = createMuiTheme({
 });
 
 function DarkTheme() {
+  Meteor.startup(() => {
+    navigator.serviceWorker.register('/sw.js')
+    .then()
+    .catch(error => console.log('ServiceWorker registration failed: ', err));
+  });
+  
   return (
     <MuiThemeProvider theme={theme}>
       <AppContainer />
