3af2241c05fee9143058124eed3390b70541c349
Esteban Gabriel Fernandez <esteban.fernandez93@outlook.com.ar>
2019-09-13 16:46:54 -0300

    Graficos solo de egresos

--- imports/ui/containers/GraficosContainer.jsx
+++ imports/ui/containers/GraficosContainer.jsx
@@ -13,7 +13,7 @@ export default withTracker(({cuadernoSeleccionada, cambiarTitulo, desde, hasta})
       cuadernoSeleccionada.cuadernoVinculado : cuadernoSeleccionada._id) : undefined;
   
   const publicHandleMovimientos = Meteor.subscribe(
-    'movimientos', cuadernoId, (new Date(desde)).getTime(), (new Date(hasta)).getTime());
+    'movimientos.egresos', cuadernoId, (new Date(desde)).getTime(), (new Date(hasta)).getTime());
   const loadingMovimientos = !publicHandleMovimientos.ready();
   const movimientos = Movimientos.find({}, { sort: { fecha: 1 } }).fetch();
   const movimientosExists = !loadingMovimientos && !!movimientos;
